ca6ecfa71fe71d42308a32139331a358e1f70155
devesht21 <deveshtalegaonkar2018@gmail.com>
2024-03-05 12:03:39 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cellsegmentation",
+    version="0.0.0",
+    author="Devesh",
+    author_email="deveshtalegaonkar2018@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
